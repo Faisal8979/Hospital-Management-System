7536500f0438cf74bcba8b593b7dfa5d0e90ea43
Faisal8979 <faisal897913@gmail.com>
2024-09-09 16:09:35 +0530

    main

--- server/index.js
+++ server/index.js
@@ -1,15 +1,43 @@
-import app from "./app.js";
-const port = process.env.PORT || 8000;
-import cloudinary from 'cloudinary';
+import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import { dbConnection } from './database/dbConnection.js';
+import MessageRouter from "./router/MessageRouter.js";
+import { errorMiddlewere } from './middlewere/errorMiddlewere.js';
+import UserRouter from './router/UserRouter.js';
+import AppointmentRouter from './router/AppointmentRouter.js';
+import morgan from 'morgan';
+ 
+const app = express();
+config({ path: "./config/config.env" })
 
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+app.use(
+    cors({
+        origin: [process.env.REACT_APP_URL, process.env.DASHBOARD_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+)
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+}));
+app.use("/api/v1/message", MessageRouter);
+app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/appointment", AppointmentRouter);
+dbConnection();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello!!</h1>")
 })
 
+app.use(errorMiddlewere);
 
-app.listen(port, () => {
-    console.log(`Server Run Port Number ${port}`);
-    
-})
+export default app;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+import app from "./index.js";
+const port = process.env.PORT || 8000;
+import cloudinary from 'cloudinary';
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+app.listen(port, () => {
+    console.log(`Server Run Port Number ${port}`);
+    
+})
